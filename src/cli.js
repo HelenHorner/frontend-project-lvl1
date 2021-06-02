9680707afda25d2a904d3406f3b60b15487b1d49
HelenHorner <volkoff-tie@yandex.ru>
2021-06-02 19:53:23 +0300

    new content README.md

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,5 @@
 
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'; //импорт библиотеки для чтения пользовательского ввода.
 
 // Wait for user's response.
 var userName = readlineSync.question('May I have your name? ');
