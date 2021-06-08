9e56d9cd5a82ae2bad565f26a8585a80cb38c57d
HelenHorner <volkoff-tie@yandex.ru>
2021-06-08 13:39:51 +0300

    game brain-gcd

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 import userName from '../src/cli.js';
-console.log(`Answer "yes" if given number is prime. Otherwise answer "no"`);
+
+console.log('Answer "yes" if given number is prime. Otherwise answer "no"');
 
 const randomInteger = (min, max) => {
   const rand = min + Math.random() * (max + 1 - min);
